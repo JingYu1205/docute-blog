acfde9b32ee656deae187b11a4f4bf65c35af526
JingYu1205 <732461427@qq.com>
2022-11-06 09:50:56 +0800

    auto update https://github.com/JingYu1205/docute.blog.git

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+echo '--------upload files start--------'
+# 进入生成的构建文件夹
+# cd ./
+
+# git init
+
+git add .
+
+git status
+
+git commit -m 'auto update https://github.com/JingYu1205/docute.blog.git'
+
+echo '--------commit successfully--------'
+
+git push -u https://github.com/JingYu1205/docute.blog.git master
+
+echo '--------push to GitHub successfully--------'
